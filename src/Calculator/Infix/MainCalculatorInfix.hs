0820ed1ca9e0ad231b7a2a06f5246ddd577ee3e0
Your Name <kevin.kds80@gmail.com>
2021-08-21 16:01:14 -0300

    Add calculator flow

--- src/Calculator/Infix/MainCalculatorInfix.hs
+++ src/Calculator/Infix/MainCalculatorInfix.hs
@@ -1,2 +1,12 @@
 module Calculator.Infix.MainCalculatorInfix where
 
+import Calculator.Infix.SintaticAnalyser
+
+import Calculator.Infix.CalculatorInfixTokens 
+import Calculator.Infix.SemanticAnalyser
+
+main = do
+    let list = startSintaticAnalysis "30+1*2+3/3-1"
+    let final = semanticAnalysis list 0
+    print list
+    print final
