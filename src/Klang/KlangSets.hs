6f8800c18566fae77533fdfbeffa69f90c3bff72
Your Name <kevin.kds80@gmail.com>
2021-08-29 13:32:51 -0300

    Add klang sets to lexical analysis

---
+++ src/Klang/KlangSets.hs
@@ -0,0 +1,24 @@
+module Klang.KlangSets where
+
+--import Klang.LexicalAnalyserKlang
+
+{- getAllTokens [] _ _ = []
+getAllTokens program line col = token:getAllTokens remain nline ncol
+    where
+        (token, remain, nline, ncol) = startAutomaton program line col [] -}
+
+identifierSet :: [Char]
+identifierSet = ['A'..'z'] ++ ['_']
+
+spaces :: [Char]
+spaces = [' ']
+
+lineBreaks :: [Char]
+lineBreaks = ['\n', '\r']
+
+endInput :: [Char]
+endInput = spaces ++ lineBreaks
+
+integers :: [Char]
+integers = ['0'..'9']
+        
