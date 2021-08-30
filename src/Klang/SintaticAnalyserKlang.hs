3bd6ad9c2bbda3b9946b76d3e77d829c3f453df6
Your Name <kevin.kds80@gmail.com>
2021-08-29 21:35:50 -0300

    Add changes to determine what symbols should be passed to IR builder

--- src/Klang/SintaticAnalyserKlang.hs
+++ src/Klang/SintaticAnalyserKlang.hs
@@ -17,7 +17,7 @@ symbolTable = [] -}
 
 startSintaticAnalysis [] _ _ = []
 startSintaticAnalysis program line col
-    | fst token == LetToken  = token:_identifier remain nline ncol
+    | fst token == LetToken  = _identifier remain nline ncol
     | otherwise = error ("Expected let. Received:" ++ show token)
     where
         (token, remain, nline, ncol) = startAutomaton program line col []
@@ -31,14 +31,16 @@ _identifier program line col
 
 _assignment [] line col = error "Unexpected EOF"
 _assignment program line col 
-    | fst token == AssignToken = token:_value remain nline ncol
+    | fst token == AssignToken = _value remain nline ncol
     | otherwise = error ("Expected AssignToken. Received:" ++ show token)
     where
         (token, remain, nline, ncol) = startAutomaton program line col [] 
 
 _value [] line col = error "Unexpected EOF"
 _value program line col 
-    | fst token == IntegerToken || fst token == StringToken = 
+    | fst token == IntegerToken 
+    || fst token == StringToken 
+    || fst token == IdentifierToken = 
         token:startSintaticAnalysis remain nline ncol
     | otherwise = error ("Expected identifier. Received:" ++ show token)
     where
