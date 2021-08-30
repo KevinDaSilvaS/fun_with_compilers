c402497c90383b24747f89e052acc199f4ae9c7c
Your Name <kevin.kds80@gmail.com>
2021-08-30 20:37:18 -0300

    Add content to runProject to execute the compilation process

--- src/Klang/RunProject.hs
+++ src/Klang/RunProject.hs
@@ -1,4 +1,13 @@
 module Klang.RunProject where
 
-import Klang.IRBuilder
+import Klang.SemanticAnalyserKlang
 
+import Klang.IRBuilder
+import Klang.SintaticAnalyserKlang
+import Klang.CodeGenerationKlang
+
+main = do
+    let t = startSintaticAnalysis "let v := 32 let a := v" 1 0 
+    let pt = createParseTree t
+    let ir = startSemanticAnalysis ([], []) pt
+    makeFile ir
