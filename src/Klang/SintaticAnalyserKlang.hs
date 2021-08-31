df0f08c436b9396f1ac3b957062d06b3fcab9e61
Your Name <kevin.kds80@gmail.com>
2021-08-31 20:51:25 -0300

    Add arithmetic support to sintatic analysis

--- src/Klang/SintaticAnalyserKlang.hs
+++ src/Klang/SintaticAnalyserKlang.hs
@@ -2,6 +2,7 @@ module Klang.SintaticAnalyserKlang where
 
 import Klang.LexicalAnalyserKlang ( startAutomaton )
 import Klang.TokensKlang
+import Klang.KlangSets
 
 {- newtype SymbolTable = Var ([Char], (Tokens, [Char]))
 symbolTable = [] -}
@@ -17,7 +18,8 @@ symbolTable = [] -}
 
 startSintaticAnalysis [] _ _ = []
 startSintaticAnalysis program line col
-    | fst token == LetToken  = _identifier remain nline ncol
+    | fst token == LetToken = _identifier remain nline ncol
+    | snd token `elem` arithmeticOperators = token:_value remain nline ncol
     | otherwise = error ("Expected let. Received:" ++ show token)
     where
         (token, remain, nline, ncol) = startAutomaton program line col []
@@ -38,10 +40,17 @@ _assignment program line col
 
 _value [] line col = error "Unexpected EOF"
 _value program line col 
-    | fst token == IntegerToken 
-    || fst token == StringToken 
-    || fst token == IdentifierToken = 
-        token:startSintaticAnalysis remain nline ncol
-    | otherwise = error ("Expected identifier. Received:" ++ show token)
+    | token == StringToken 
+    && nextTokenValue `elem` arithmeticOperators = 
+        error ("Cannot perform arithmetic operation:" 
+        ++ nextTokenValue ++ " in a non integer token.") 
+    | token == IntegerToken 
+    || token == StringToken 
+    || token == IdentifierToken = 
+        (token, value):startSintaticAnalysis remain nline ncol
+    | otherwise = error ("Expected identifier. Received:" ++ show (token, value))
     where
-        (token, remain, nline, ncol) = startAutomaton program line col [] 
+
+        ((token, value), remain, nline, ncol) = startAutomaton program line col [] 
+        ((_, nextTokenValue), _, _, _) = startAutomaton remain line col [] 
+ 
