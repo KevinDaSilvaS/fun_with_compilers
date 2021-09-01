03b36a84194fdd0113621aae55da4b93bbbb5c4b
Your Name <kevin.kds80@gmail.com>
2021-09-01 20:43:01 -0300

    Made changes to run with new files

--- src/Klang/RunProject.hs
+++ src/Klang/RunProject.hs
@@ -6,24 +6,27 @@ import Klang.IRBuilder
 import Klang.SintaticAnalyserKlang
 import Klang.CodeGenerationKlang
 import Klang.LexicalAnalyserKlang
+import Klang.IRBuilderKlang
 
 main = do
     let t = startSintaticAnalysis "let v := 32 let a := v let r := \"oi\"" 1 0 
     let pt = createParseTree t
-    let ir = startSemanticAnalysis ([], []) pt
-    makeFile ir
+    --let ir = startSemanticAnalysis ([], []) pt
+    --makeFile pt
+    print "end"
 
 sintaticAnalysisOnly = 
     startSintaticAnalysis "let v := 32 + 2 - 1 * 3 let a := v / 2" 1 0 
 
 sintaticAndIr = do
     let t = startSintaticAnalysis "let v := 32 + 2 - 1 * 3 let a := v / 2" 1 0
-    let pt = createParseTree t
-    --let ir = startSemanticAnalysis ([], []) pt
+    let pt = createKlangParseTree t
     print pt
+    let ir = startSemanticAnalysis [] pt
+    print $ show ir
 
 getAllTokens = do 
-    let t = getToken "let v := 32 + 2 - 1 * 3 let a := v / 2" 1 0 
+    let t = getToken "let v := 32 + 2 - 1 * 3 let a := v / 2 let id := \"oi\" + 2" 1 0 
     print t
 
 getToken [] l c = []
