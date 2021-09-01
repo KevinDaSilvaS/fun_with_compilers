93a5b2c0b4dde7e7deb2a1cf9ee750505cba6f88
Your Name <kevin.kds80@gmail.com>
2021-09-01 20:43:42 -0300

    Start new semantic analysis process to parse arithmetic expressions

--- src/Klang/SemanticAnalyserKlang.hs
+++ src/Klang/SemanticAnalyserKlang.hs
@@ -1,13 +1,38 @@
+{-# LANGUAGE DataKinds #-}
 module Klang.SemanticAnalyserKlang where
 
-import Klang.IRBuilder
+import Klang.KlangSets
 import Klang.TokensKlang
 
-startSemanticAnalysis :: ([[Char]], [[Char]]) -> ParsingTree -> ([[Char]], [[Char]])
+import Klang.IRBuilderKlang
+import Data.Maybe (isJust)
+
+startSemanticAnalysis symbolTable (Assign 
+    (idToken, identifier) (Expr expr) ptr)
+    | isJust existentIdentifier  = 
+        error ("Identifier " ++ identifier ++ " already declared") 
+    | otherwise = startSemanticAnalysis nSymbolTable ptr
+    where
+        existentIdentifier = lookup identifier symbolTable
+        nSymbolTable = symbolTable ++ [(identifier, parseExpr symbolTable expr)]
+startSemanticAnalysis st ptr = st
+
+
+parseExpr st (Integer (IntegerToken, value) op) = 0;
+parseExpr st expr = 1;
+--startSemanticAnalysis st ptr = error ""
+
+{- startSemanticAnalysis :: ([[Char]], [[Char]]) -> ParsingTree -> ([[Char]], [[Char]])
 startSemanticAnalysis symbolTable (Program ptr ptr') = output
     where
         nSymbolTable = startSemanticAnalysis symbolTable ptr
         output = startSemanticAnalysis nSymbolTable ptr'
+startSemanticAnalysis symbolTable
+    (Arithmetic (tokenOperator, operator) (tokenValue, value) ptr)
+        | tokenValue == IdentifierToken && notElem value identifiersList =
+            error ("Identifier " ++ show value ++ " not defined.")
+        where
+            (identifiersList, valuesList) = symbolTable
 startSemanticAnalysis symbolTable (Assign (_, identifier) (tokenValue, value) ptr)
     | tokenValue == IdentifierToken && notElem value identifiersList =
         error ("Identifier " ++ show value ++ " not defined.")
@@ -16,4 +41,5 @@ startSemanticAnalysis symbolTable (Assign (_, identifier) (tokenValue, value) pt
         (identifiersList, valuesList) = symbolTable
         nSymbolTable = (identifier:identifiersList, value:valuesList)
 startSemanticAnalysis st EndNode = st
-startSemanticAnalysis _ _ = error "Unexpected error in semantic analysis"
+startSemanticAnalysis _ _ = error "Unexpected error in semantic analysis" -}
+
