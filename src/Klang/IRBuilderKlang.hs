cc21093724c2eab7d56808c3558ff2addb52f53a
Your Name <kevin.kds80@gmail.com>
2021-09-04 18:07:03 -0300

    Add if and comparative operations to parseTree

--- src/Klang/IRBuilderKlang.hs
+++ src/Klang/IRBuilderKlang.hs
@@ -3,31 +3,49 @@
 module Klang.IRBuilderKlang where
 
 import Klang.TokensKlang
+import Klang.KlangSets
 
 data Expr = Expr Expr  |
             Integer (Tokens, String) Expr  |
             Str (Tokens, String)           |
             Operator (Tokens, String) Expr |
+            ComparativeExpr Expr (Tokens, String) Expr |
             EndExpr deriving (Show, Eq)
 
 data ParsingTree = Program ParsingTree |
                    Assign (Tokens, String) Expr ParsingTree |
                    Show (Tokens, String) Expr ParsingTree   |
+                   If (Tokens, String)
+                        Expr (Tokens, String) ParsingTree   |
+                   CloseBlock (Tokens, String) ParsingTree     |
                    EndNode
                    deriving (Show, Eq)
 
 createKlangParseTree [] = EndNode
-createKlangParseTree ((LetToken, value):xs)  = createKlangParseTree xs
-createKlangParseTree ((ShowToken, value):xs) = 
+createKlangParseTree ((LetToken, value):xs) = createKlangParseTree xs
+createKlangParseTree ((StartBlockToken, value):xs) =
+    error "Unexpected open block symbol"
+createKlangParseTree ((CloseBlockToken, ";"):xs) =
+    CloseBlock (CloseBlockToken, ";") (createKlangParseTree xs)
+createKlangParseTree ((ConditionalToken, value):xs) =
+    If (ConditionalToken, value)
+        expr'
+        openBlock
+        (createKlangParseTree (tail remain'))
+    where
+        (expr, remain) = parseExpression xs
+        (expr', remain') = parseComparativeExpressions expr remain
+        openBlock = head remain'
+createKlangParseTree ((ShowToken, value):xs) =
     Show (ShowToken, value) expr (createKlangParseTree remain)
     where
         (expr, remain) = parseExpression xs
 createKlangParseTree ((IdentifierToken, value):xs)
     | nextToken == IdentifierToken || nextToken == IntegerToken =
-        Assign (IdentifierToken, value) 
+        Assign (IdentifierToken, value)
             (Expr (Integer (nextToken, nextValue) expr))
         (createKlangParseTree remain)
-    | nextToken == StringToken = Assign 
+    | nextToken == StringToken = Assign
         (IdentifierToken, value) (Str (nextToken, nextValue))
         (createKlangParseTree (tail xs))
     where
@@ -40,24 +58,76 @@ parseExpression ((IntegerToken, value):xs)    =
     where
         (expr, remain) = parseExpression xs
 parseExpression ((IdentifierToken, value):xs) =
-    (Integer (IdentifierToken, value) expr, remain)
+        (Integer (IdentifierToken, value) expr,
+        remain)
     where
         (expr, remain) = parseExpression xs
-parseExpression ((t, "*"):xs) = (Operator (t, "*") expr, remain)
+parseExpression ((t, "*"):xs) =
+        (Operator (t, "*") expr,
+        remain)
     where
         (expr, remain) = parseExpression xs
-parseExpression ((t, "/"):xs) = (Operator (t, "/") expr, remain)
+parseExpression ((t, "/"):xs) =
+    (Operator (t, "/") expr,
+    remain)
     where
         (expr, remain) = parseExpression xs
-parseExpression ((t, "+"):xs) = (Operator (t, "+") expr, remain)
+parseExpression ((t, "+"):xs) =
+    (Operator (t, "+") expr,
+    remain)
     where
         (expr, remain) = parseExpression xs
-parseExpression ((t, "-"):xs) = (Operator (t, "-") expr, remain)
+parseExpression ((t, "-"):xs) =
+    (Operator (t, "-") expr,
+    remain)
     where
         (expr, remain) = parseExpression xs
-parseExpression ((StringToken, value):xs) = 
-    (Str (StringToken, value), remain)
+parseExpression ((StringToken, value):xs) =
+    parseComparativeExpressions
+    (Str (StringToken, value))
+    remain
     where
         (expr, remain) = parseExpression xs
+{- parseExpression ((_, value):xs)
+    | value `elem` comparativeOperators = 
+        error "Cannot compare String or Int type with Boolean comparison" -}
 parseExpression xs = (EndExpr, xs)
 
+parseComparativeExpressions expr ((token, ">="):xs) =
+    (ComparativeExpr expr (token, ">=") expr', remain')
+    where
+        (expr', remain) = parseExpression xs
+        remain' = sanitizeExtraComparativeExpression remain
+parseComparativeExpressions expr ((token, ">"):xs)  =
+    (ComparativeExpr expr (token, ">") expr', remain')
+    where
+        (expr', remain) = parseExpression xs
+        remain' = sanitizeExtraComparativeExpression remain
+parseComparativeExpressions expr ((token, "<="):xs) =
+    (ComparativeExpr expr (token, "<=") expr', remain')
+    where
+        (expr', remain) = parseExpression xs
+        remain' = sanitizeExtraComparativeExpression remain
+parseComparativeExpressions expr ((token, "<"):xs)  =
+    (ComparativeExpr expr (token, "<") expr', remain')
+    where
+        (expr', remain) = parseExpression xs
+        remain' = sanitizeExtraComparativeExpression remain
+parseComparativeExpressions expr ((token, "!="):xs) =
+    (ComparativeExpr expr (token, "!=") expr', remain')
+    where
+        (expr', remain) = parseExpression xs
+        remain' = sanitizeExtraComparativeExpression remain
+parseComparativeExpressions expr ((token, "=="):xs) =
+    (ComparativeExpr expr (token, "==") expr', remain')
+    where
+        (expr', remain) = parseExpression xs
+        remain' = sanitizeExtraComparativeExpression remain
+parseComparativeExpressions expr xs = (expr, xs)
+
+sanitizeExtraComparativeExpression ((token, value):xs)
+    | value `elem` comparativeOperators = 
+        error 
+        ("Cannot compare String or Int type with Boolean comparison: " 
+        ++ value ++ " in: " ++ show xs) 
+sanitizeExtraComparativeExpression xs = xs
