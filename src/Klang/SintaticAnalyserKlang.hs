161081ba02f77de2fddb5a20b0bc377793cbb8a8
Your Name <kevin.kds80@gmail.com>
2021-09-04 18:08:06 -0300

    Add if and comparative operations to sintatic analyser

--- src/Klang/SintaticAnalyserKlang.hs
+++ src/Klang/SintaticAnalyserKlang.hs
@@ -16,31 +16,44 @@ symbolTable = [] -}
     V -> int or string
 -}
 
-startSintaticAnalysis [] _ _ = []
-startSintaticAnalysis program line col
-    | fst token == LetToken = token:_identifier remain nline ncol
-    | fst token == ShowToken = token:_value remain nline ncol
-    | snd token `elem` arithmeticOperators = token:_value remain nline ncol
+startSintaticAnalysis [] _ _ 0 = []
+startSintaticAnalysis program line col closingBlocks
+    | fst token == LetToken  = token:_identifier remain nline ncol closingBlocks
+    | fst token == ShowToken = token:_value remain nline ncol closingBlocks
+    | fst token == StartBlockToken && closingBlocks >= 0 = 
+        token:startSintaticAnalysis remain nline ncol (closingBlocks+1)
+    | fst token == CloseBlockToken && closingBlocks >= 0 = 
+        token:startSintaticAnalysis remain nline ncol (closingBlocks-1)
+    | fst token == StartBlockToken || fst token == CloseBlockToken = 
+        error "Unbalanced open close blocks check for inconsistencies with the :(open block) and ; (close block) symbols"
+    | fst token == ConditionalToken = 
+        token:_value remain nline ncol closingBlocks
+    | snd token `elem` arithmeticOperators = 
+        token:_value remain nline ncol closingBlocks
+    | snd token `elem` comparativeOperators = 
+        token:_value remain nline ncol closingBlocks
+    | fst token == EmptyToken = []
     | otherwise = error ("Expected let. Received:" ++ show token)
     where
         (token, remain, nline, ncol) = startAutomaton program line col []
 
-_identifier [] line col = error "Unexpected EOF"
-_identifier program line col 
-    | fst token == IdentifierToken = token:_assignment remain nline ncol
+_identifier [] line col _ = error "Unexpected EOF"
+_identifier program line col closingBlocks
+    | fst token == IdentifierToken = 
+        token:_assignment remain nline ncol closingBlocks
     | otherwise = error ("Expected identifier. Received:" ++ show token)
     where
         (token, remain, nline, ncol) = startAutomaton program line col [] 
 
-_assignment [] line col = error "Unexpected EOF"
-_assignment program line col 
-    | fst token == AssignToken = _value remain nline ncol
+_assignment [] line col _ = error "Unexpected EOF"
+_assignment program line col closingBlocks
+    | fst token == AssignToken = _value remain nline ncol closingBlocks
     | otherwise = error ("Expected AssignToken. Received:" ++ show token)
     where
         (token, remain, nline, ncol) = startAutomaton program line col [] 
 
-_value [] line col = error "Unexpected EOF"
-_value program line col 
+_value [] line col _ = error "Unexpected EOF"
+_value program line col closingBlocks
     | token == StringToken 
     && nextTokenValue `elem` arithmeticOperators = 
         error ("Cannot perform arithmetic operation:" 
@@ -48,7 +61,7 @@ _value program line col
     | token == IntegerToken 
     || token == StringToken 
     || token == IdentifierToken = 
-        (token, value):startSintaticAnalysis remain nline ncol
+        (token, value):startSintaticAnalysis remain nline ncol closingBlocks
     | otherwise = error ("Expected identifier. Received:" ++ show (token, value))
     where
         ((token, value), remain, nline, ncol) = startAutomaton program line col [] 
