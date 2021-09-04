546ce6012894a90b8bf172c467b4aa19e8c752b5
Your Name <kevin.kds80@gmail.com>
2021-09-04 18:08:36 -0300

    Change inputs

--- src/Klang/RunProject.hs
+++ src/Klang/RunProject.hs
@@ -9,7 +9,7 @@ import Klang.LexicalAnalyserKlang
 import Klang.IRBuilderKlang
 
 main = do
-    let t = startSintaticAnalysis "let v := 32 let a := v let r := \"oi\"" 1 0 
+    let t = startSintaticAnalysis "let v := 32 let a := v let r := \"oi\"" 1 0 0
     let pt = createParseTree t
     --let ir = startSemanticAnalysis ([], []) pt
     --makeFile pt
@@ -19,12 +19,13 @@ sintaticAnalysisOnly =
     startSintaticAnalysis "let v := 32 + 2 - 1 * 3 show v + a let a := v / 2" 1 0 
 
 sintaticAndIr = do
-    let t = startSintaticAnalysis "let v := 32 + 2 - 1 * 3 let a := v / 2 let r := \"oi\" show 9 + 2 show v + a show \"ola\" let s := v + a" 1 0
+    let t = startSintaticAnalysis " if 5 < 2 + 3 : if 6 > 2 : show 5 ; show 4 ; " 1 0 0
+        {- let v := 32 + 2 - 1 * 3 let a := v / 2 let r := \"oi\" show 9 + 2 show v + a show \"ola\" let s := v + a -}
     let pt = createKlangParseTree t
     print pt
-    let st = startSemanticAnalysis [] pt
-    print $ show st
-    makeFile st pt
+    --let st = startSemanticAnalysis [] pt
+    --print $ show st
+    --makeFile st pt
     --print t
 
 getAllTokens = do 
