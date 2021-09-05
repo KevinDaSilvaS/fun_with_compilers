2ebf50972d01c40c170f6f8c7db11e2590ef558a
Your Name <kevin.kds80@gmail.com>
2021-09-05 12:44:47 -0300

    Add klang valid file and output

---
+++ src/Klang/klang_parsed.js
@@ -0,0 +1,21 @@
+const v_ = "132";
+if ( (v_) < ("2 + 3") ) { 
+if ( (6) > (2) ) { 
+console.log( 5 );
+}
+console.log( 4 );
+}
+const v = 31.0;
+const a = 15.5;
+const r = "oi";
+console.log( 9 + 2 );
+console.log( v * a + 5 );
+console.log( "ola" );
+const s = 46.5;
+for (let _$loop = 0; _$loop < 10; _$loop++) {
+console.log( "ola mundo" );
+console.log( _$loop );
+}
+
+
+
