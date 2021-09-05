3c1b449e7146029238a59319c7c77d6eec406992
Your Name <kevin.kds80@gmail.com>
2021-09-05 12:44:31 -0300

    Add routine token

--- src/Klang/TokensKlang.hs
+++ src/Klang/TokensKlang.hs
@@ -14,6 +14,7 @@ data Tokens = LetToken         |
               EqualityToken    |
               NotEqualToken    |
               ConditionalToken |
+              RoutineToken     |
               StartBlockToken  |
               CloseBlockToken  |
               OpenParenToken   |
