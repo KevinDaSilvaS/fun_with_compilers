1f2e09b0158ca9bdd7950610f646a48af8069504
Your Name <kevin.kds80@gmail.com>
2021-09-07 18:54:23 -0300

    Start klang reimplementation

---
+++ src/Compiler/RunProject.hs
@@ -0,0 +1,21 @@
+module Compiler.RunProject where
+
+import Compiler.LexicalAnalyser ( startAutomaton )
+import Compiler.TokensKlang
+import Compiler.SintaticAnalyser
+
+getAllTokens = do
+    --let t = parseResult (startAutomaton "#let $a := 12 + * - / \"oi\"" 1 0 [])
+    let t = getToken "let a := 12 + * - / \"oi\" : ; >= if > < != == <= routine if_ t5" 1 0
+    print t
+
+getToken [] l c = []
+getToken xs l c = (token, value):getToken program line col
+    where
+        ((token, value), line, col, program) =  startAutomaton xs line col []
+
+sintaticAnalysis = do
+    let program = "let a := 12 / 2 - readLine if a <= 12-2 : routine 3*2 : show 42 ; ;"
+        --"let a := 12 + * - / \"oi\" : ; >= if > < != == <= routine if_ t5"
+    let r = startSintaticAnalysis program 1 0 0
+    print r
